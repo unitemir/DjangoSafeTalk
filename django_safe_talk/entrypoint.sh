ac4399e33af563821b7e78f0188b6be73108fcd5
unitemir <unitemir@gmail.com>
2023-11-11 14:05:10 +0600

    Added PostgreSQL client installation for database readiness check

--- django_safe_talk/entrypoint.sh
+++ django_safe_talk/entrypoint.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
 # Проверка доступности базы данных перед выполнением миграций
-# Это полезно, когда используется база данных в отдельном сервисе (например, Postgres в Docker Compose)
-# и нужно дождаться её полного запуска
 echo "Waiting for database..."
+# Увеличим количество попыток и интервал ожидания
+max_attempts=30  # Количество попыток подключения
+attempt=1        # Счетчик попыток
+
+# Используем цикл для проверки доступности порта и готовности базы
 while ! nc -z db 5432; do
-  sleep 0.1
+  if [ $attempt -ge $max_attempts ]; then
+    echo "Database is not available after $max_attempts attempts... exiting."
+    exit 1
+  fi
+
+  echo "Database not ready yet. Attempt $attempt of $max_attempts..."
+  sleep 2  # Увеличиваем интервал ожидания
+  attempt=$(( $attempt + 1 ))
 done
 echo "Database started"
+
+# После успешного подключения к порту, проверим с помощью pg_isready
+echo "Checking if PostgreSQL is ready..."
+while ! pg_isready -h db -p 5432 -U $POSTGRES_USER; do
+  if [ $attempt -ge $max_attempts ]; then
+    echo "PostgreSQL is not ready after $max_attempts attempts... exiting."
+    exit 1
+  fi
+
+  echo "PostgreSQL not ready yet. Attempt $attempt of $max_attempts..."
+  sleep 2
+  attempt=$(( $attempt + 1 ))
+done
+
+echo "PostgreSQL is ready."
+
 
 # Применение миграций
 python manage.py migrate
