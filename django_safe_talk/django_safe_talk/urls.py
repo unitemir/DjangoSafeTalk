7e916ba8ee9b0853a1e1e0e05781a1d335469564
unitemir <unitemir@gmail.com>
2023-11-11 13:12:56 +0600

    ref urls

--- django_safe_talk/django_safe_talk/urls.py
+++ django_safe_talk/django_safe_talk/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/chats/', include('chat.urls')),
+    path('chats/', include('chat.urls')),
 ]
